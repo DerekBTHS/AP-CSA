cca295082890e627abf6f076f9aac6c01ea3c559
Derek <derektdp44@gmail.com>
2024-09-09 19:00:03 -0400

    Lesson 1.1 complete

---
+++ Lesson 1.1/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.err.print("Hello, my name is ");
+        System.out.println("Derek Chen");
+        System.err.println("║║╔║║╔╗ ║");
+        System.err.println("╠╣╠║║║║ ║");
+        System.err.println("║║╚╚╚╚╝ O");
+    }
+}
