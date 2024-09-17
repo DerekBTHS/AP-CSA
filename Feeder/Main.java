0a77df0982a14c4b2c45ea3e0e8dadf8b11438a7
Derek <derektdp44@gmail.com>
2024-09-17 12:16:55 -0400

    Completed test cases in Main

--- Feeder/Main.java
+++ Feeder/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        // simulate cases as shown in part a
+        System.out.println("Test cases as shown in part a: ");
         Feeder a1 = new Feeder(500);
         a1.simulateOneDay(12);
         System.out.println(a1.getCurrentFood());
@@ -10,5 +10,13 @@ public class Main {
         Feeder a3 = new Feeder(100);
         a3.simulateOneDay(5);
         System.out.println(a3.getCurrentFood());
+
+        System.out.println("Test cases as shown in part b: ");
+        Feeder b1 = new Feeder(2400);
+        System.out.println(b1.simulateManyDays(10, 4));
+        Feeder b2 = new Feeder(250);
+        System.out.println(b2.simulateManyDays(10, 5));
+        Feeder b3 = new Feeder(0);
+        System.out.println(b3.simulateManyDays(5, 10));
     }
 }
