ad365df2e4321403e0ba04acb5d8221c7654602c
DerekBTHS <derekc101@nycstudents.net>
2025-03-14 09:29:20 -0400

    finished ArrayResizer

---
+++ ArrayResizer/ArrayResizer.java
@@ -0,0 +1,47 @@
+public class ArrayResizer {
+    /** Returns true if and only if every value in row r of array2D is non-zero.
+    * Precondition: r is a valid row index in array2D.
+    * Postcondition: array2D is unchanged.
+    */
+    public static boolean isNonZeroRow(int[][] array2D, int r) {
+        /* to be implemented in part (a) */
+        for (int num : array2D[r]) {
+            if (num == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    /** Returns the number of rows in array2D that contain all non-zero values.
+    * Postcondition: array2D is unchanged.
+    */
+    public static int numNonZeroRows(int[][] array2D) {
+        /* implementation not shown */
+        int count = 0;
+        for (int row = 0; row < array2D.length; row++) {
+            if (isNonZeroRow(array2D, row)) {
+                count++;
+            }
+        }
+        return count;
+    }
+    /** Returns a new, possibly smaller, two-dimensional array that contains only rows
+    * from array2D with no zeros, as described in part (b).
+    * Precondition: array2D contains at least one column and at least one row with no zeros.
+    * Postcondition: array2D is unchanged.
+    */
+    public static int[][] resize(int[][] array2D) {
+        /* to be implemented in part (b) */
+        int[][] smaller = new int[numNonZeroRows(array2D)][array2D[0].length];
+        int currentRow = 0;
+        for (int row = 0; row < array2D.length; row++) {
+            if (isNonZeroRow(array2D, row)) {
+                for (int col = 0; col < array2D[row].length; col++) {
+                    smaller[currentRow][col] = array2D[row][col];
+                }
+                currentRow++;
+            }
+        }
+        return smaller;
+    }
+}
